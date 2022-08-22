f90c7f2b78cb16d6eb8ecf4888c5fdd20376f110
Yarden Dahan <68491359+Jordan-me@users.noreply.github.com>
2022-08-22 13:03:57 +0300

    fix merge

--- client.py
+++ client.py
@@ -9,62 +9,10 @@ width = 700
 height = 700
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
-<<<<<<< HEAD
+
 bg = pygame.image.load("assets/menuScreen3.jpg")
 bg = pygame.transform.scale(bg, (width, height))
 bg_loading = pygame.transform.scale(pygame.image.load("assets/waitScreen.gif"), (width, height))
-
-
-# class Button:
-#     def __init__(self, text, x, y, color):
-#         self.text = text
-#         self.x = x
-#         self.y = y
-#         self.color = color
-#         self.width = 150
-#         self.height = 100
-#
-#     def draw(self, win):
-#         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-#         font = pygame.font.SysFont("comicsans", 40)
-#         text = font.render(self.text, 1, (255, 255, 255))
-#         win.blit(text, (self.x + round(self.width / 2) - round(text.get_width() / 2),
-#                         self.y + round(self.height / 2) - round(text.get_height() / 2)))
-#
-#     def click(self, pos):
-#         x1 = pos[0]
-#         y1 = pos[1]
-#         if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
-#             return True
-#         else:
-#             return False
-=======
-
-#Hi
-class Button:
-    def __init__(self, text, x, y, color):
-        self.text = text
-        self.x = x
-        self.y = y
-        self.color = color
-        self.width = 150
-        self.height = 100
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        font = pygame.font.SysFont("comicsans", 40)
-        text = font.render(self.text, 1, (255,255,255))
-        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
-
-    def click(self, pos):
-        x1 = pos[0]
-        y1 = pos[1]
-        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
-            return True
-        else:
-            return False
->>>>>>> 183596a7e0d04f9b438a82b5b037fa2c024376a6
-
 
 def redrawWindow(win, game, p):
     win.fill(color = (138,51,36))
